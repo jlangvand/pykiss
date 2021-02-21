62047cac407791aec17400d6d3c24ef51fb5caa6
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-02-21 16:43:47 +0100

    Update packaging configuration

--- setup.py
+++ setup.py
@@ -4,19 +4,29 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pykiss-orbitntnu",
-    version="0.0.1",
-    author="Joakim Skogø Langvand @jlangvand",
-    author_email="jlangvand@gmail.com",
+    name="pykiss",
+    version="0.0.3",
+    author="Orbit NTNU",
+    author_email="cto@orbitntnu.com",
     description="Simple serial KISS library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jlangvand/pykiss",
     classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Topic :: Communications :: Ham Radio",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: POSIX :: Linux",
     ],
-    packages=setuptools.find_packages(),
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     python_requires='>=3.6',
+    install_requires=['pyserial'],
+    project_urls={
+        "Orbit NTNU": "https://orbitntnu.com",
+    },
 )

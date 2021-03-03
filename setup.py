71ed24eacc34b7b331b05ac5df54294938845835
Joakim Skogø Langvand <jlangvand@gmail.com>
2021-03-03 18:26:58 +0100

    Push minor version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name="pykiss",
-    version="0.1.0",
+    version="0.1.1",
     author="Orbit NTNU",
     author_email="cto@orbitntnu.com",
     description="Simple serial KISS library",
